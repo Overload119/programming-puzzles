8cf79f7fa4377e26e8921bc6be58b479668ba924
Amir Sharif <budgeneration@gmail.com>
2013-05-03 21:42:25 -0300

    Create facebook-2.js

---
+++ facebook-2.js
@@ -0,0 +1,34 @@
+var result = [];
+
+function permutate(string, index) {
+  if(index === string.length - 1) {
+    result.push(newString);
+  } else {
+    permutate(string, index + 1);
+
+    for(var i = index + 1; i < string.length; i++) {
+      var currentLetter = string[index];
+      var swapLetter = string[i];
+
+      // Javascript doesn't modify strings in place
+      var newString = '';
+      for( var j = 0; j < string.length; j++ ) {
+        if( j === index ) {
+          newString += swapLetter;
+        } else if ( j === i ) {
+          newString += currentLetter;
+        } else {
+          newString += string[j];
+        }
+      }
+
+      permutate(newString, index + 1);
+    }
+  }
+}
+
+function permutateString(string) {
+  result = [];
+  permutate(string, 0);
+  return result;
+}
