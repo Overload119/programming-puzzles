ae27acd14c9ec3d37491d34c8763f1e1d8b2e3c4
Amir Sharif <budgeneration@gmail.com>
2013-05-10 10:22:42 -0400

    Create facebook-4.js

---
+++ facebook-4.js
@@ -0,0 +1,11 @@
+// Implement a square root function without the use of Math.sqrt
+
+function sqrt(num) {
+  if (num < 0) {
+  }
+  var guessLow = 0;
+  while( guessLow < guessHigh ) {
+    var guessMiddle = (guessHigh + guessLow) / 2
+    if( guessLow
+  }
+}
